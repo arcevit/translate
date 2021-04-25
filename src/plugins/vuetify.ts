e5499391c6a34b7987ff49802118e4498c868458
arcevit <hakkisuman@gmail.com>
2021-04-25 12:53:18 +0200

    theme colors added

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,7 +1,22 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib/framework';
+import Vue from "vue";
+import Vuetify from "vuetify/lib/framework";
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: "#2C3FD6",
+        secondary: "#BFEDE5",
+        accent: "#8c9eff",
+        error: "#b71c1c",
+        defaultGreen: "#29C1A7",
+        lightYellow: "#BFEDE5",
+        defaultBlack: "#454A5B",
+        ligthGrey: "#C3C3C3",
+        defaultBackground: "#F3F6FA",
+      },
+    },
+  },
 });
